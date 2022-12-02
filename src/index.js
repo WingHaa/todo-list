1503d7e52418a685e6a60e43221c685a11fbd56c
WingHaa <quanlm24@gmail.com>
2022-12-02 08:08:50 -0800

    Add import statement for header and sidebar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
 import './style.css';
 console.log("Hello World!");
+import {
+  header
+} from './components/header';
+// import {
+//   navSideBar
+// } from './components/side-nav-bar';
+
+
+window.addEventListener('DOMContentLoaded', renderHomePage);
+
+function renderHomePage() {
+  const body = document.querySelector('body');
+  header.render(body);
+  navSideBar.render(body);
+  console.log('i run')
+};
+
+export const pubsub = {
+  events: [],
+  add: function (event, method) {
+    this.events[event] = this.events[event] || []
+    this.events[event].push(method)
+  },
+  remove: function (event, method) {
+    if (this.events[event]) this.events[event] = this.events[event].filter(fn => fn !== method)
+  },
+  emit: function (event, data) {
+    this.events[event].forEach(method => {
+      method(data);
+    });
+  }
+}
