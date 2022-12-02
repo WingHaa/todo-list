8f8cf8e66573270cafa541acfff695662bc261db
WingHaa <quanlm24@gmail.com>
2022-12-02 08:07:38 -0800

    Add import header and side bar for testing

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,3 +1,28 @@
+import {header} from './components/header';
+import {navSideBar} from './components/side-nav-bar';
+
+
+window.addEventListener('DOMContentLoaded', renderHomePage);
+function renderHomePage() {
+  // renderHeader();
+  // renderNavSideBar();
+
+  // // create a container for project or todo viewing
+  // const content = document.createElement('main');
+  // content.classList = 'content lg:ml-72 ml-5';
+  // body.appendChild(content);
+
+  // renderProjectPage();
+  // renderTodoPage(); // for testing remove this later
+  // renderModalOverlay();
+  // renderProjectModal();
+  // renderTodoModal();
+  const body = document.querySelector('body');
+  header.render(body);
+  navSideBar.render(body);
+  console.log('i run')
+};
+
 export const pubsub = {
   events: [],
   add: function (event, method) {
@@ -5,7 +30,7 @@ export const pubsub = {
     this.events[event].push(method)
   },
   remove: function (event, method) {
-    if (this.events[event]) this.events[event] = this.events[event].filter(element => element !== method)
+    if (this.events[event]) this.events[event] = this.events[event].filter(fn => fn !== method)
   },
   emit: function (event, data) {
     this.events[event].forEach(method => {
