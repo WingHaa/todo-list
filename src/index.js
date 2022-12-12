f328270807c2bbe47c911d43399110855fa8e1fb
WingHaa <quanlm24@gmail.com>
2022-12-11 18:28:50 -0800

    Rewrote some styles and init functions to commodate to changes in new logic for components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
+/* beautify preserve:start */
 import './style.css';
-console.log("Hello World!");
-import {
-  header
-} from './components/header';
-// import {
-//   navSideBar
-// } from './components/side-nav-bar';
-
+import { projectModule } from './project';
+import { todoModule } from './todo';
+import { header } from './components/site-header';
+import { navSideBar } from './components/side-nav-bar';
+import { projectHeader } from './components/project-header';
+import { projectContent } from './components/project-content';
+import { todoHeader } from './components/todo-header';
+import { inboxHeader } from './components/inbox-header';
+import { todayHeader } from './components/today-header';
+import { upcomingHeader } from './components/upcoming-header';
+import { todoContent } from './components/todo-content';
+import { todoFooter } from './components/todo-footer';
+import { projectModal } from './components/add-project-modal';
+import { todoModal } from './components/add-todo-modal';
+import { confirmDeletionModal } from './components/confirmation';
+/* beautify preserve:end */
 
 window.addEventListener('DOMContentLoaded', renderHomePage);
-
 function renderHomePage() {
   const body = document.querySelector('body');
   header.render(body);
-  navSideBar.render(body);
-  console.log('i run')
+  navSideBar.render();
+  projectModule.init();
+  todoModule.init();
+  inboxHeader.init();
+  todayHeader.init();
+  upcomingHeader.init();
+  projectHeader.init();
+  todoHeader.init();
+
+  projectContent.init();
+  todoContent.init();
+
+  todoFooter.init();
+
+  projectModal.init();
+  todoModal.init();
+  confirmDeletionModal.init();
+
+  pubsub.emit('serveInboxHeader')
+  pubsub.emit('queryTodo', {projectId: 'inbox'})
+  pubsub.emit('serveTodoFooter');
 };
 
 export const pubsub = {
@@ -31,4 +58,4 @@ export const pubsub = {
       method(data);
     });
   }
-}
+};
